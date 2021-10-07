52f758b712fb5dba210f8cadf7d23c2152b0e6d4
benterem <benterem@gmail.com>
2021-10-07 16:20:18 +0300

    has

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
+// Object is essentially hash table in JS has o(1) to check if exists
 const zipCodes = {}
 
 
 app.get('/display', (request, response) => {
   const zip = Number(request.params.id)
   response.status(200).send(zipCodes)
+})
+
+app.get('/has/:id', (request, response) => {
+  const zip = Number(request.params.id)
+  response.status(200).send(zip in zipCodes)
 })
 
 app.post('/insert/:id', (request, response) => {
