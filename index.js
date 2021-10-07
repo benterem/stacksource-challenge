fa90e699123e747134ef052b58a757287c431b51
benterem <benterem@gmail.com>
2021-10-07 16:18:14 +0300

    display v1

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 
+const zipCodes = {}
+
+
+app.get('/display', (request, response) => {
+  const zip = Number(request.params.id)
+  response.status(200).send(zipCodes)
+})
+
 app.post('/insert/:id', (request, response) => {
   const zip = Number(request.params.id)
+  zipCodes[zip] = zip
   response.status(201).send(`Zip code ${zip} inserted`)
 })
+
+
 
 const PORT = 3000
 app.listen(PORT, () => {
